90cb47d6f2f06102f0248de8ac77768b0bef1da1
Mirdalan <michrzan@gmail.com>
2017-12-11 20:04:36 +0100

    started adding ajax requests

--- player_site.py
+++ player_site.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 flask_app = Flask(__name__)
 
 
 @flask_app.route("/")
 def main_page():
     return render_template('main.html')
+
+
+@flask_app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
