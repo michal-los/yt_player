805d8d32ac7a5678fc0f6b68f53fbb2b0eb2ae76
Mirdalan <michrzan@gmail.com>
2018-01-19 23:10:32 +0100

    updating tests

--- tests.py
+++ tests.py
@@ -10,10 +10,19 @@ class MockZmq(MagicMock):
         return self
 
 
+def recv_play():
+    return {
+        'command': 'play',
+        'video_id': ''
+    }
+
+
 class PlayerServiceTestCase(unittest.TestCase):
 
     @patch('zmq.Context')
-    def test_sends_play_command(self, MockZmq):
+    @patch('zmq.sugar.socket.Socket.recv_pyobj')
+    @patch('player_service.MyPlayer.play')
+    def test_sends_play_command(self, MockZmq, recv_play, MagicMock):
         player = MyPlayer()
         player.wait_for_command()
         self.assertEqual(player.status_meta_data['status'], "playing")
