c722dd5f1b36e472d6d933439dbf1b2f7e2c123f
chrzanom <m.chrzanowski@tt.com.pl>
2018-01-19 18:57:49 +0100

    Finished refactoring. Starting some tests.

--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
 import unittest
+from unittest.mock import patch, Mock
+
+from player_service import MyPlayer
 
 
 class PlayerServiceTestCase(unittest.TestCase):
 
-    def test_sends_play_command(self):
-        pass
+    @patch('zmq.sugar.socket.Socket')
+    def test_sends_play_command(self, zmq_socket):
+        player = MyPlayer()
+        player.wait_for_command()
 
 
 if __name__ == '__main__':
